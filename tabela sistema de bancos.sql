a00f7d90968773a4cc05344ede51ec7f730324d8
Maria Luíza Dantas <maludantass123@gmail.com>
2025-09-23 09:54:54 -0300

    chore: inserindo tuplas em Cliente

--- tabela sistema de bancos.sql
+++ tabela sistema de bancos.sql
@@ -14,6 +14,39 @@ CREATE TABLE Cliente (
     CEP VARCHAR(15) CHECK (LENGTH(CEP) BETWEEN 8 AND 15) 
 );
 
+INSERT INTO Cliente (idCliente, nome, rua, numero, CEP) VALUES
+(1,  'Leonardo Almeida', 'Aldeia', '101', '50000111'),
+(2,  'Maria Clara Holanda', 'Aldeia', '102', '50000112'),
+(3,  'Rosélio', 'Barra de Jangada', '103', '50000113'),
+(4,  'Lucas', 'Camaragibe', '104', '50000114'),
+(5,  'Julia Nunes Cordeiro', 'Camaragibe', '105', '50000115'),
+(6,  'Breno Tavares', 'Encruzilhada', '106', '50000116'),
+(7,  'Adelia', 'Madalena', '107', '50000117'),
+(8,  'Nina Zilá', 'Madalena', '108', '50000118'),
+(9,  'Mariana Guimarães Coêlho', 'Monteiro', '109', '50000119'),
+(10, 'Maria Luíza Dantas Tavares', 'Monteiro', '110', '50000120'),
+(11, 'Henrique', 'Casa Forte', '111', '50000121'),
+(12, 'Maria Regina Moraes', 'Casa Forte', '112', '50000122'),
+(13, 'Julya Micaely da Silva', 'Casa Forte', '113', '50000123'),
+(14, 'Marcela Coutinho Cavalcanti', 'Casa Forte', '114', '50000124'),
+(15, 'Victor', 'Dois Irmãos', '115', '50000125'),
+(16, 'Lídia', 'Ilha do Leite', '116', '50000126'),
+(17, 'Arthur Rocha Campos', 'Poço da Panela', '117', '50000127'),
+(18, 'Irvin', 'Poço da Panela', '118', '50000128'),
+(19, 'Clara Amorim', 'Poço da Panela', '119', '50000129'),
+(20, 'Antonio', 'Pina', '120', '50000130'),
+(21, 'Maria Luiza', 'Hipódromo', '121', '50000131'),
+(22, 'Guilherme', 'Rosarinho', '122', '50000132'),
+(23, 'Sofia Duarte', 'Rosarinho', '123', '50000133'),
+(24, 'Maria Isabel Freire Pessoa Raposo', 'Boa Viagem', '124', '50000134'),
+(25, 'Isabela Lima Alves Dantas', 'Boa Viagem', '125', '50000135'),
+(26, 'Bruno Feitosa', 'Boa Viagem', '126', '50000136'),
+(27, 'Juliana Lima', 'Boa Viagem', '127', '50000137'),
+(28, 'Luisa Martins', 'Torre', '128', '50000138'),
+(29, 'Miguel Rabelo', 'Torre', '129', '50000139'),
+(30, 'Fabiana', 'Monteiro', '130', '50000140'),
+(31, 'Julia Maria', 'Casa Forte', '131', '50000141');
+
 CREATE TABLE Solicitacao (
     idSolicitacao INT PRIMARY KEY
 ); 
